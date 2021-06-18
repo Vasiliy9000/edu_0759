f6ed9e271fc0e05eb9fc081a90c845d0546154b8
Vasiliy9000 <mr.nightexpres@gmail.com>
2021-06-18 04:59:37 +0300

    Задача 4,2 - getName и Age не используются

---
+++ task_4.2/Dog.java
@@ -0,0 +1,21 @@
+public class Dog {
+    //напишите тут ваш код
+    public String name = "пэс";
+    public int age = 0;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -14,11 +14,11 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Dog dog1 = new Dog();
+        dog1.setAge(2);
+        dog1.setName("Жужик");
+        System.out.println(dog1.name);
+        System.out.println(dog1.age);
+        }
     }
-}
-
-public class Dog {
-    //напишите тут ваш код
-}
 
