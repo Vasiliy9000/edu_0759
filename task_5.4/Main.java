1ebb37307f0149021a2418287f83c32ab6d85e71
Vasiliy9000 <mr.nightexpres@gmail.com>
2021-07-03 01:04:59 +0300

    Доделал

--- task_5.4/Main.java
+++ task_5.4/Main.java
@@ -1,5 +1,4 @@
-  
-/* 
+/*
 1. Ввести путь к файлу с консоли.
 2. Прочитать из него набор чисел.
 3. Вывести на консоль только четные, отсортированные по возрастанию.
@@ -19,8 +18,48 @@
 10
 */
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к файлу");
+        String stringPath = scanner.nextLine();
+        System.out.println("Вы ввели путь:" + stringPath);
+
+        ArrayList<Integer> sortedList = new ArrayList<Integer>();
+
+        if (stringPath.contentEquals("test")) stringPath = "c:\\temp\\test.txt";
+        File file = new File(stringPath);
+
+
+        if (file.exists()) {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String text;
+                try {
+                    while (((text = bufferedReader.readLine()) != null)) {
+
+                        Integer i = Integer.parseInt(text);
+
+                        //   System.out.println("text="+text +" int="+ i);
+                        if (i != null)
+                            if (i.intValue() > 0)
+                                sortedList.add(i);
+
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            System.out.println("\n Несортированный список больше нуля:\n" + sortedList);
+            Collections.sort(sortedList);
+            System.out.println("\n Сортированный список больше нуля:\n" + sortedList);
+        } else System.out.println("Файл не найден");
+
     }
 }
